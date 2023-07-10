0fc09f4d061b390edde7e7c3c899916c9bdc2a39
vlsulliv <vsulliv@pm.me>
2023-07-10 14:54:57 -0500

    implement handlebars

--- app.js
+++ app.js
@@ -1,22 +1,47 @@
-
+// load express module
 const express = require('express');
+const handlebars = require('express-handlebars');
 const app = express();
-const port = process.env.PORT || 5000;
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
+const port = 3000;
+
+// load handlebar into module
+
+
+// set app to use handlebar engine
+// app.set('view engine', 'handlebars');
+app.set('view engine', 'hbs');
+
+
+// set handlebar configurations
+app.engine('hbs', handlebars({
+  layoutsDir: __dirname + '/views/layouts',
+  extname: 'hbs',
+  //new configuration parameter
+  defaultLayout: 'planB',
+  partialsDir: __dirname + '/views/partials/'
+}));
+
+// serves static import, import css file
+app.use(express.static('public'));
+
+//Sets a basic route
+app.get('/', (req, res) => {
+  // instead of res.render('main', {layout : 'index'});
+  res.render('main');
 });
-app.use((req, res, next) => {
-  res.send('Welcome to Express');
-});
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+
+app.engine('hbs', handlebars({
+  layoutsDir: __dirname + '/views/layouts',
+  extname: 'hbs',
+  //new configuration parameter
+  defaultLayout: 'planB',
+  }));
+  app.get('/', (req, res) => {
+  //instead of res.render('main', {layout: 'index'});
+  res.render('main');
 });
 
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+//Makes the app listen to port 3000
+app.listen(port, () => console.log(`App listening to port 
+${port}`));
